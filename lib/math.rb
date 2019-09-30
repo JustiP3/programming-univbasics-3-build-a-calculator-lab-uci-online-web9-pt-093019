0b4c6d8167b4965f912c59393d676c90c005acfc
Justin Phillips <justinphil02@yahoo.com>
2019-09-30 01:12:15 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,26 @@
 def addition(num1, num2)
-
+num1 + num2 
 end
 
 def subtraction(num1, num2)
-
+num1 - num2 
 end
 
 def division(num1, num2)
-
+num1 / num2
 end
 
 def multiplication(num1, num2)
-
+num1 * num2
 end
 
 def modulo(num1, num2)
-
+base = num1 / num2
+float1 = num1.to_f 
+float2 = num2.to_f
+rem = (float1 / float2) - base
 end
 
 def square_root(num)
-
+num.math.sqrt
 end
